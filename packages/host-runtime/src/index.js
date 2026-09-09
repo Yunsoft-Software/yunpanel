1775d8367bebe41abf04d45a5bb8cf1c9f759475
raichubuilds <120168234+raichubuilds@users.noreply.github.com>
2026-09-09 16:09:14 +0300

    feat: export host service inspectors

--- packages/host-runtime/src/index.js
+++ packages/host-runtime/src/index.js
@@ -1,4 +1,7 @@
 export { inspectHostInventory, inventoryInternals } from './inventory.js';
+export { createDockerInspector, inspectDocker, parseDockerPsOutput } from './docker-inspector.js';
+export { createNginxInspector, inspectNginx, parseNginxConfigMetadata } from './nginx-inspector.js';
+export { inspectAllowlistedServices, parseSystemdProperties, systemdInspectionPolicy } from './systemd-inspector.js';
 export {
   createSystemPackageManager,
   systemPackageManager,
