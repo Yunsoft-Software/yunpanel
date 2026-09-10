b1398d25324370e7c3a1b6d97a1412b12babd1d1
raichubuilds <120168234+raichubuilds@users.noreply.github.com>
2026-09-11 00:46:18 +0300

    fix: expose migration preview conflicts as 409

--- apps/api/src/website-migration-preview.js
+++ apps/api/src/website-migration-preview.js
@@ -1,10 +1,11 @@
 const UUID_PATTERN = /^[0-9a-f]{8}-[0-9a-f]{4}-[1-5][0-9a-f]{3}-[89ab][0-9a-f]{3}-[0-9a-f]{12}$/i;
 
 export class WebsiteMigrationPreviewError extends Error {
-  constructor(code, message) {
+  constructor(code, message, status = 409) {
     super(message);
     this.name = 'WebsiteMigrationPreviewError';
     this.code = code;
+    this.status = status;
   }
 }
 
