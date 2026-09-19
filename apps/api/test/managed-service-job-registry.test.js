8f09e4a0330c626e70fc428a0adc6346556b750e
raichubuilds <120168234+raichubuilds@users.noreply.github.com>
2026-09-19 05:23:36 +0300

    test(api): align sqlite mail service packages

--- apps/api/test/managed-service-job-registry.test.js
+++ apps/api/test/managed-service-job-registry.test.js
@@ -6,7 +6,9 @@ import { createJobRegistry, JobRegistryError } from '../src/job-registry.js';
 const serverId = 'server-1';
 const servicePackages = {
   nginx: ['nginx'], mariadb: ['mariadb-server'], mysql: ['mysql-server'], docker: ['docker.io'], cron: ['cron'],
-  postfix: ['postfix'], dovecot: ['dovecot-imapd', 'dovecot-lmtpd', 'dovecot-sieve'], rspamd: ['rspamd'],
+  postfix: ['postfix', 'postfix-sqlite', 'sqlite3'],
+  dovecot: ['dovecot-imapd', 'dovecot-lmtpd', 'dovecot-sieve', 'dovecot-sqlite', 'sqlite3'],
+  rspamd: ['rspamd'],
   roundcube: ['roundcube-core', 'roundcube-sqlite3', 'php-fpm'], phpmyadmin: ['phpmyadmin', 'php-fpm', 'php-mysql'],
   elfinder: ['php-fpm', 'php-mbstring', 'php-zip', 'libjs-jquery', 'libjs-jquery-ui'],
   postsrsd: ['postsrsd'],
